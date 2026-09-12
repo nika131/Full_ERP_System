7e30bafeb6d44f70571d8d7fd5daca1c98381b45
nika131 <nmegrelishvili313@gmail.com>
2026-09-12 17:43:48 +0400

    added pos backend engine

---
+++ NexusERP/NexusERP.Api/Controllers/PosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NexusERP.Api.Controllers
+{
+    public class PosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ NexusERP/NexusERP.Application/Interfaces/Services/IPosService.cs
@@ -0,0 +1,18 @@
+﻿using NexusERP.Application.DTOs;
+using NexusERP.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NexusERP.Application.Interfaces.Services
+{
+    internal interface IPosService
+    {
+        Task<Shift> OpenShiftAsync(int userId, OpenShiftDto dto);
+        Task<Shift> CloseShiftAsync(int shiftId, int userId, CloseShiftDto dto);
+        Task<CashMovement> AddCashMovementAsync(int shiftId, int userId, CashMovementDto dto);
+        Task<Receipt> ProcessCheckoutAsync(int userId, CheckoutRequestDto cart);
+    }
+}
